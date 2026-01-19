83ba339210712247d3168c552b3350f104eef74b
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-01-19 15:59:42 +0530

    fetched audio file and played

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import { useEffect, useState } from "react"
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const apiKey = "1c7e8f56";
+  const [track, setTract] = useState(null);
+  const CLIENT_ID = "1c7e8f56";
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://www.theaudiodb.com/api/v1/json/123/searchtrack.php?s=coldplay&t=yellow")
+        const response = await fetch(`https://api.jamendo.com/v3.0/albums/tracks/?client_id=${CLIENT_ID}&format=jsonpretty&limit=1&artist_name=we+are+fm&type=album+single`)
         
         const resData = await response.json();
-        console.log(resData)
-        setData(resData)
+
+        const firstTrack = resData.results[0].tracks[0];
+        
+        setTract(firstTrack)
       } catch (err) {
         console.log(err)
       }
     }
 
     fetchData()
-  }, [])
+  }, [CLIENT_ID])
 
   return (
     <>
       <button className="px-3 m-2 border-2 rounded-sm cursor-pointer">Click</button>
-      <audio controls src=""></audio>
+      {track && <audio controls src={track.audio}></audio>}
     </>
   )
 }
