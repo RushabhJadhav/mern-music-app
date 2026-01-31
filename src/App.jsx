bdf362ec968631187693cc0023323fd3181e14c6
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-01-31 18:46:14 +0530

    added axios and savan api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
   const [track, setTract] = useState(null);
-  const CLIENT_ID = "1c7e8f56";
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`https://api.jamendo.com/v3.0/albums/tracks/?client_id=${CLIENT_ID}&format=jsonpretty&limit=1&artist_name=we+are+fm&type=album+single`)
-        
-        const resData = await response.json();
+        const res = await axios.get(`https://saavn.sumit.co/api/songs/Eng0tSqR`);
 
-        const firstTrack = resData.results[0].tracks[0];
+        const resData = res.data;
+
+        const firstTrack = resData.data[0].downloadUrl[4];
         
         setTract(firstTrack)
       } catch (err) {
@@ -20,12 +20,12 @@ const App = () => {
     }
 
     fetchData()
-  }, [CLIENT_ID])
+  }, [])
 
   return (
     <>
       <button className="px-3 m-2 border-2 rounded-sm cursor-pointer">Click</button>
-      {track && <audio controls src={track.audio}></audio>}
+      {track && <audio controls src={track.url}></audio>}
     </>
   )
 }
